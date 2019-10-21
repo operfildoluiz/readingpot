90494aa7ccbfa786628037efadc06e3d7362323a
Luiz <dev.luizh@gmail.com>
2019-10-21 17:30:17 -0300

    Updates

---
+++ precache-manifest.41ae77e83409c620da5a82ea5efae4bb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bce8b990680234dc49de686c2ee8effb",
+    "url": "/readingpot/index.html"
+  },
+  {
+    "revision": "d6b937a7766edddc7998",
+    "url": "/readingpot/static/js/2.f0792e35.chunk.js"
+  },
+  {
+    "revision": "9cbd2522fd7d2631a070",
+    "url": "/readingpot/static/js/main.98660936.chunk.js"
+  },
+  {
+    "revision": "9ac3bc24886a657430f4",
+    "url": "/readingpot/static/js/runtime-main.ff36c574.js"
+  }
+]);
